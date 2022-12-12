9162f484716ff50d4f445f4288e132323aad8120
J.Henrique <jhenriqueribeirodesa@gmail.com>
2022-12-12 15:28:49 -0300

    add my hidden card show on hover

--- frontend/src/Card.tsx
+++ frontend/src/Card.tsx
@@ -13,9 +13,11 @@ import Ten from './svg/10.svg';
 import J from './svg/j.svg';
 import Q from './svg/q.svg';
 import K from './svg/k.svg';
+import { ReactElement, useState } from 'react';
 
 type CardProps = {
   card: Card | string;
+  isHidden?: boolean;
   className?: string;
 };
 
@@ -36,12 +38,21 @@ const CardImages = {
   [CardType.K]: K,
 };
 
-export const CardElement = ({ card, className }: CardProps) => {
-  return <div className={`border-gray-500 border-solid border-2 w-20 rounded-md ${className}`}>
-    {
-      typeof card === 'string' ? <img alt='A card face down' src={CardImages.Back} className='w-full h-full' /> 
-      : <img alt={`A card of playing card of value ${card.cardType}`} src={CardImages[card.cardType]} className='w-full h-full' />
-    }
+export const CardElement = ({ card, className, isHidden = false }: CardProps) => {
+  const [isMouseOverCard, setIsMouseOverCard] = useState(false);
+  let cardImage: ReactElement;
+  if (typeof card === 'string' || (isHidden && !isMouseOverCard)) {
+    cardImage = <img alt='A card face down' src={CardImages.Back} className='w-full h-full' />;
+  } else {
+    cardImage = <img alt={`A card of playing card of value ${card.cardType}`} src={CardImages[card.cardType]} className='w-full h-full' />;
+  }
+
+  function changeOverState(status: boolean) {
+    return () => setIsMouseOverCard(status);
+  }
+
+  return <div style={{borderWidth: 1}} onMouseOut={changeOverState(false)} onMouseOver={changeOverState(true)} className={`border-gray-700 border-solid w-20 rounded-md overflow-hidden ${className}`}>
+    { cardImage }
   </div>;
 }
 
